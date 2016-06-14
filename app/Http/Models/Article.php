46becab0314417be205173a6a11e8056fab90b8e
suning920 <suning920@sina.com>
2016-06-14 23:43:53 +0800

    修改大小问题在linux不能正常运行的问题

--- app/Http/Models/Article.php
+++ app/Http/Models/Article.php
@@ -16,7 +16,7 @@ class Article extends Model
     }
 
     public function category() {
-        return $this->belongsTo('App\Http\Models\category', 'category_id', 'id');
+        return $this->belongsTo('App\Http\Models\Category', 'category_id', 'id');
     }
 
 
